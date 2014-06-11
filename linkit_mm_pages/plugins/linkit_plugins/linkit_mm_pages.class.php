8ee43de52b57d015a7d10f9eb0deca22e47d7839
Jay Dansand <dansandj@799552.no-reply.drupal.org>
2014-06-11 08:03:02 -0500

    Update mm_tweaks/linkit_mm_pages to work with new Linkit 3.x API.

--- linkit_mm_pages/plugins/linkit_plugins/linkit_mm_pages.class.php
+++ linkit_mm_pages/plugins/linkit_plugins/linkit_mm_pages.class.php
@@ -3,17 +3,25 @@
  * Plugin class.
  */
 
-
-class LinkitMMPagesPlugin  extends LinkitPlugin {
+class LinkitMMPagesPlugin extends LinkitSearchPlugin {
+  /**
+   * Overrides LinkitSearchPlugin::ui_title().
+   */
+  // function ui_title() {
+    // return t('Monster Menus pages');
+  // }
+  
+  /**
+   * Overrides LinkitSearchPlugin::ui_description().
+   */
+  // function ui_description() {
+    // return t('Extend Linkit with file support (Managed files).');
+  // }
   /**
    * The autocomplete callback function for the Linkit Entity plugin.
    */
-  function autocomplete_callback() {
+  public function fetchResults($search_string) {
     $matches = array();
-    // There is a huge typo in the Linkit API, so until a patch is committed the
-    // search string is actually stored as "serach_string".  This check gives us
-    // future compatibility:
-    $search_string = (isset($this->serach_string)) ? $this->serach_string : $this->search_string;
     // Split up the search string on spaces, individually db_like() each chunk,
     // then glue back together with % to turn a string from "ab cd" to "ab%cd"
     // for use in our query building:
@@ -30,7 +38,7 @@ class LinkitMMPagesPlugin  extends LinkitPlugin {
       MM_GET_TREE_MMTID => mm_home_mmtid(),
       MM_GET_TREE_DEPTH => -1,
       MM_GET_TREE_RETURN_PERMS => MM_PERMS_READ,
-      // MM_GET_TREE_SORT => TRUE,
+      MM_GET_TREE_SORT => FALSE,
       MM_GET_TREE_RETURN_TREE => TRUE,
       MM_GET_TREE_WHERE => 'name LIKE \'%' . $db_like . '%\'',
     );
@@ -40,16 +48,33 @@ class LinkitMMPagesPlugin  extends LinkitPlugin {
     // with "ORDER BY NULL", but we're using the power of REGEX to allow some
     // flexibility.  Basically, the query can end with any ORDER statement within
     // 5 words of the end of the string, or no ORDER statement at all.
-    $query = preg_replace('/(ORDER(\s+\w+){2,5}$|$)/', ' AND '.MM_PERMS_READ.' > 0 \\1', $query, 1);
+    $query = preg_replace('/(ORDER(\s+\w+){2,5}$|$)/', ' AND '.MM_PERMS_READ.' > 0 \\1', $query, 1, $count);
     $results = db_query($query);
-    
+      /**
+   * Fetch search results based on the $search_string.
+   *
+   * @param $search_string
+   *   A string that contains the text to search for.
+   *
+   * @return
+   *   An associative array whose values are an
+   *   associative array containing:
+   *   - title: A string to use as the search result label.
+   *   - description: (optional) A string with additional information about the
+   *     result item.
+   *   - path: The URL to the item.
+   *   - group: (optional) A string with the group name for the result item.
+   *     Best practice is to use the plugin name as group name.
+   *   - addClass: (optional) A string with classes to add to the result row..
+   */
     foreach ($results AS $page) {
       $url = mm_content_get_mmtid_url($page->mmtid);
-      $display_url = preg_replace('|([^\/]{5}).+?\/|', '\\1.../', url($url));
+      $display_url = preg_replace('|([^\/]{10}).+?\/|', '\\1.../', url($url));
       $matches[] = array(
-        'title' => $this->buildLabel($page->name) . ' (' . $display_url . ')',
-        'path' => $this->buildPath($url),
-        'group' => $this->buildGroup('Pages'),
+        'title' => check_plain($page->name),
+        'path' => $url,
+        'description' => t('Path: @url', array('@url' => $display_url)),
+        'group' => t('MM Pages'),
       );
     }
     return $matches;
